1a3888f3713658403e70986a1258482f0b8e6594
Raneem <aljamal.raneem@gmail.com>
2023-11-23 01:41:50 +0200

    create the home page, add functionallty to app and home ande set socket client

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,37 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css"
+import { useState } from "react";
+import io from "socket.io-client";
+import "./App.css";
 import Login from "./Pages/Login/login";
-import Home from "./Pages/Home/index";
+import Home from "./Pages/Home/home";
+
+const socket = io.connect("http://localhost:4000");
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+
   return (
     <>
-     <BrowserRouter>
-     <>
-        <div className="App">
-              <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/Home" element={<Home />} />
-              </Routes>
-        </div>
+      <BrowserRouter>
+        <>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route
+                path="/Home"
+                element={
+                  <Home
+                    username={username}
+                    setUsername={setUsername}
+                    room={room}
+                    setRoom={setRoom}
+                    socket={socket}
+                  />
+                }
+              />
+            </Routes>
+          </div>
         </>
       </BrowserRouter>
     </>
