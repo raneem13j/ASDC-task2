e841c95088f974ff46f31d291d5de3d20876b8c3
Raneem <aljamal.raneem@gmail.com>
2023-11-24 20:05:41 +0200

    add download function to message controller to download files

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,8 +1,16 @@
 import Message from "../models/messageModel.js";
 import upload from "../middlewares/multer.js";
 import { v2 as cloudinary } from "cloudinary";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import dotenv from "dotenv";
+import path from "path";
+import fs from "fs";
 dotenv.config();
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const sendMessage = async (req, res) => {
   try {
@@ -44,5 +52,26 @@ export const getMessageByRoomId = async (req, res) => {
     res.status(200).json(messages);
   } catch (err) {
     res.status(500).json(err);
+  }
+};
+
+export const fileDownload = async (req, res) => {
+
+  const filename = req.params.filename;
+  const filePath = path.join(__dirname, "..", "uploads", filename); 
+
+  try {
+    // Check if the file exists
+    if (fs.existsSync(filePath)) {
+      // Provide the file for download
+      res.sendFile(filePath);
+    } else {
+      // If the file does not exist, return a 404 Not Found status
+      res.status(404).send("File not found");
+    }
+  } catch (err) {
+    // Handle any errors that occur during the process
+    console.error(err);
+    res.status(500).send("Internal Server Error");
   }
 };
--- server/routes/messageRoute.js
+++ server/routes/messageRoute.js
@@ -5,11 +5,13 @@ import upload from '../middlewares/multer.js';
 
 import {
   getMessageByRoomId,
+  fileDownload,
   sendMessage,
   } from "../controllers/messageController.js";
 
 
 router.get('/:roomId', getMessageByRoomId);
+router.get('/download/:filename',fileDownload);
 router.post('/',upload.single("file"), sendMessage);
 
 
